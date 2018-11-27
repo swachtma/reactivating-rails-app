0f525e3f1681241ae66461e2b71e126f880b2fd6
Scott Wachtmann <swachtma@gmail.com>
2018-11-27 09:56:06 -0500

    Add build/ directory to git tracking

---
+++ client/build/precache-manifest.b865353d2cbc8793b2aa194f5285e99a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aed17ca7f0827124d62c4b0739b0eda5",
+    "url": "/static/media/reactivating-rails.aed17ca7.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dce2aca883cbc4dd0689",
+    "url": "/static/js/main.dce2aca8.chunk.js"
+  },
+  {
+    "revision": "ffd6a7e8cb37e968e239",
+    "url": "/static/js/1.ffd6a7e8.chunk.js"
+  },
+  {
+    "revision": "dce2aca883cbc4dd0689",
+    "url": "/static/css/main.0a32ddca.chunk.css"
+  },
+  {
+    "revision": "ec104c3f5ead457815a3ccd06744446b",
+    "url": "/index.html"
+  }
+];
